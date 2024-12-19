bae230b1ac650d4a2cefec87881a56a1c8810b45
Mark Mcloughlin <mark25mky@gmail.com>
2024-12-19 15:49:24 +0000

    added shopping bag url pattern

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('', views.home_view, name='index'),
+    path('shopping-bag/', views.shopping_bag, name='shopping_bag'),
 ]
