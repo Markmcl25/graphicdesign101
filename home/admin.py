b512a428540f07ab33f156c39e8b02446ad16108
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-02 21:09:04 +0000

    registered model to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PortfolioProject
 
-# Register your models here.
+@admin.register(PortfolioProject)
+class PortfolioProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date', 'client')
+    search_fields = ('title', 'description', 'category')
+    list_filter = ('category', 'date')
