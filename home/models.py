141584f46cc70ce0bef34c1fd382cf6ab27d41a0
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-02 21:04:35 +0000

    created model for portfolio project

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class PortfolioProject(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='portfolio_images/')
+    category = models.CharField(max_length=100, blank=True, null=True)
+    client = models.CharField(max_length=100, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    tools = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
