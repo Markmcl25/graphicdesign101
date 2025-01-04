f5e265f57df62fee6c51b83e731230937c35fb18
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-04 22:12:19 +0000

    registered models in admin to manage orders

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import PortfolioProject
+from .models import Order, OrderItem
 
 @admin.register(PortfolioProject)
 class PortfolioProjectAdmin(admin.ModelAdmin):
@@ -7,3 +8,13 @@ class PortfolioProjectAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description', 'category')
     list_filter = ('category', 'date')
     fields = ('title', 'description', 'image', 'category', 'client', 'date', 'tools', 'file')
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'total_price', 'created_at')
+    list_filter = ('status',)
+    inlines = [OrderItemInline]    
