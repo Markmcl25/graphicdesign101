46be8bc299899df3953c9f4d23f8bbf8f30b0e89
Mark Mcloughlin <mark25mky@gmail.com>
2025-01-04 20:09:55 +0000

    added view for inquiries and created inquiry template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import PortfolioProject
+from .forms import InquiryForm
+from django.contrib import messages
 
 # View for the homepage and /home/
 def index(request):
@@ -28,3 +30,15 @@ def project_detail(request, project_id):
     """ A view to display details of a specific project """
     project = get_object_or_404(PortfolioProject, id=project_id)
     return render(request, 'home/project_detail.html', {'project': project})
+
+def submit_inquiry(request):
+    if request.method == 'POST':
+        form = InquiryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your inquiry has been submitted successfully!")
+            return redirect('home')  # Redirect to the homepage or thank you page
+    else:
+        form = InquiryForm()
+    
+    return render(request, 'home/inquiry_form.html', {'form': form})    
